79ffb050e8474d607612104bce8e68740f1ebf36
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2021-06-19 13:26:50 +0200

    Fixed player skin files Implemented drawing of player sprites

--- src/engine/renderer.go
+++ src/engine/renderer.go
@@ -5,29 +5,34 @@ import (
 	"github.com/faiface/pixel/pixelgl"
 	"retro-carnage/engine/characters"
 	"retro-carnage/engine/geometry"
+	"retro-carnage/logging"
 )
 
+// Renderer is used to render the current state of an GameEngine to screen
 type Renderer struct {
-	canvas              *pixelgl.Canvas
-	engine              *GameEngine
-	outputArea          *geometry.Rectangle
-	playerTileSuppliers []*characters.PlayerSpriteSupplier
-	scalingFactor       float64
-	window              *pixelgl.Window
+	canvas                *pixelgl.Canvas
+	engine                *GameEngine
+	outputArea            *geometry.Rectangle
+	playerSpriteSuppliers []*characters.PlayerSpriteSupplier
+	scalingFactor         float64
+	window                *pixelgl.Window
 }
 
+// NewRenderer creates and initializes a new Renderer instance.
 func NewRenderer(engine *GameEngine, window *pixelgl.Window) *Renderer {
-	var playerTileSuppliers = make([]*characters.PlayerSpriteSupplier, 0)
+	var playerSpriteSuppliers = make([]*characters.PlayerSpriteSupplier, 0)
 	for _, player := range characters.PlayerController.ConfiguredPlayers() {
-		playerTileSuppliers = append(playerTileSuppliers, characters.NewPlayerSpriteSupplier(player))
+		playerSpriteSuppliers = append(playerSpriteSuppliers, characters.NewPlayerSpriteSupplier(player))
 	}
 
-	var result = &Renderer{engine: engine, playerTileSuppliers: playerTileSuppliers, window: window}
+	var result = &Renderer{engine: engine, playerSpriteSuppliers: playerSpriteSuppliers, window: window}
 	result.initializeGeometry()
 	result.initializeCanvas()
 	return result
 }
 
+// Render draws the state of the GameEngine to screen. The parameter elapsedTimeInMs is used to pick the right sprites
+// for animated objects, like the player character or enemies.
 func (r *Renderer) Render(elapsedTimeInMs int64) {
 	r.drawBackground()
 	r.drawEnemies(elapsedTimeInMs)
@@ -39,6 +44,8 @@ func (r *Renderer) Render(elapsedTimeInMs int64) {
 	r.canvas.Draw(r.window, pixel.IM.Moved(r.canvas.Bounds().Center()))
 }
 
+// drawBackground draws the background image of the current mission section onto the in-memory canvas.
+// Do not call from outside this class.
 func (r *Renderer) drawBackground() {
 	for _, bg := range r.engine.Backgrounds() {
 		if nil != bg.Sprite {
@@ -51,6 +58,8 @@ func (r *Renderer) drawBackground() {
 	}
 }
 
+// drawEnemies draws sprites for each of the currently visible enemies onto the in-memory canvas.
+// Do not call from outside this class.
 func (r *Renderer) drawEnemies(elapsedTimeInMs int64) {
 	//this.engine.enemies.forEach((enemy) => {
 	//	const tile = enemy.tileSupplier.getTile(elapsedTimeInMs, enemy);
@@ -64,22 +73,29 @@ func (r *Renderer) drawEnemies(elapsedTimeInMs int64) {
 	//});
 }
 
+// drawPlayers draws sprites for each of the players onto the in-memory canvas.
+// Do not call from outside this class.
 func (r *Renderer) drawPlayers(elapsedTimeInMs int64) {
-	//const positions = this.engine.playerPositions;
-	//const behaviors = this.engine.playerBehaviors;
-	//PlayerController.getRemainingPlayers().forEach((player) => {
-	//	const tile = this.playerTileSuppliers[player.index].getTile(elapsedTimeInMs, behaviors[player.index]);
-	//	if (tile) {
-	//		const translatedPosition = tile.translate(positions[player.index]);
-	//		const canvas = tile.getCanvas();
-	//		if (canvas && this.ctx) {
-	//			this.ctx.drawImage(canvas, translatedPosition.x, translatedPosition.y);
-	//			// this.drawDebugRect(positions[player.index]);
-	//		}
-	//	}
-	//});
+	var outputAreaInverseRoot = pixel.V(r.outputArea.X, r.outputArea.Y+r.outputArea.Height)
+	for _, player := range characters.PlayerController.RemainingPlayers() {
+		var behavior = r.engine.playerBehaviors[player.Index()]
+		var spriteWithOffset = r.playerSpriteSuppliers[player.Index()].Sprite(elapsedTimeInMs, behavior)
+		if nil != spriteWithOffset {
+			var position = r.engine.playerPositions[player.Index()]
+			var spriteCenter = pixel.Vec{
+				X: spriteWithOffset.Offset.X + position.X + spriteWithOffset.Sprite.Picture().Bounds().W()/2,
+				Y: -1 * (spriteWithOffset.Offset.Y + position.Y + spriteWithOffset.Sprite.Picture().Bounds().H()/2),
+			}.Scaled(r.scalingFactor).Add(outputAreaInverseRoot)
+			var matrix = pixel.IM.Scaled(pixel.V(0, 0), r.scalingFactor).Moved(spriteCenter)
+			spriteWithOffset.Sprite.Draw(r.canvas, matrix)
+		} else {
+			logging.Warning.Printf("Player spriteWithOffset missing for player %d", player.Index())
+		}
+	}
 }
 
+// drawBullets draws the flying bullets onto the in-memory canvas.
+// Do not call from outside this class.
 func (r *Renderer) drawBullets() {
 	//const ctx = this.ctx
 	//if ctx {
@@ -90,6 +106,8 @@ func (r *Renderer) drawBullets() {
 	//}
 }
 
+// drawExplosives draws the flying explosives (grenades, RPGs) onto the in-memory canvas.
+// Do not call from outside this class.
 func (r *Renderer) drawExplosives() {
 	//this.engine.explosives.forEach((explosive) => {
 	//	const canvas = explosive.tileSupplier.getTile().getCanvas();
@@ -99,6 +117,8 @@ func (r *Renderer) drawExplosives() {
 	//});
 }
 
+// drawExplosions draws sprites for animated explosions onto the in-memory canvas.
+// Do not call from outside this class.
 func (r *Renderer) drawExplosions(elapsedTimeInMs int64) {
 	//this.engine.explosions.forEach((explosion) => {
 	//	const tile = explosion.tileSupplier.getTile(elapsedTimeInMs);
@@ -109,6 +129,9 @@ func (r *Renderer) drawExplosions(elapsedTimeInMs int64) {
 	//});
 }
 
+// drawDebugRect draws a given geometry.Rectangle onto the in-memory canvas.
+// Useful for debugging.
+// Do not call from outside this class.
 func (r *Renderer) drawDebugRect(rect geometry.Rectangle) {
 	//if (this.ctx) {
 	//	this.ctx.strokeStyle = "orange";
