12505812d0c42a87b5a55eca41d6cae9ab679c79
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-08-12 21:27:30 +0200

    Implemented death animation

--- engine/characters/tank-sprite-supplier.go
+++ engine/characters/tank-sprite-supplier.go
@@ -24,14 +24,17 @@ func (supplier *TankSpriteSupplier) GetDurationOfEnemyDeathAnimation() int64 {
 }
 
 func (supplier *TankSpriteSupplier) Sprite(msSinceLastSprite int64, enemy ActiveEnemy) *graphics.SpriteWithOffset {
-	var skinFrames = enemySkins[enemy.Skin].MovementByDirection[supplier.direction.Name]
 	if enemy.Dying {
+		var deathAnimationFrames = enemySkins[enemy.Skin].DeathAnimation[supplier.direction.Name]
 		supplier.durationSinceLastSprite += msSinceLastSprite
 
 		if supplier.durationSinceLastSprite > durationOfEnemyMovementFrame {
 			supplier.durationSinceLastSprite = 0
-			supplier.lastIndex = (supplier.lastIndex + 1) % len(skinFrames) // TODO: death animation instead of skinFrames
+			supplier.lastIndex = (supplier.lastIndex + 1) % len(deathAnimationFrames)
 		}
+		return deathAnimationFrames[supplier.lastIndex].ToSpriteWithOffset()
 	}
+
+	var skinFrames = enemySkins[enemy.Skin].MovementByDirection[supplier.direction.Name]
 	return skinFrames[supplier.lastIndex].ToSpriteWithOffset()
 }
