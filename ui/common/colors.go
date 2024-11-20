b7e836e16f502a6dd5c05f55234fa938143fc090
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2024-11-20 21:36:27 +0100

    refactoring

--- ui/common/colors.go
+++ ui/common/colors.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	Black      = ParseHexColor("#000000")
-	DirtyWhite = ParseHexColor("#f6f4e6")
+	Gray       = ParseHexColor("#ababab")
 	Green      = ParseHexColor("#12a45d")
 	LightGray  = ParseHexColor("#d3d3d3")
 	ModalBg    = ParseHexColor("#2f4f4e")
